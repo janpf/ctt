5b3f92348b0eded889627a5f2d2d7ea13fce8bea
janpf <janpf@users.noreply.github.com>
2020-06-26 17:19:21 +0200

    json creator

--- create_json.sh
+++ create_json.sh
@@ -2,5 +2,6 @@
 for f in keys/*
 do
   echo parsing $f
-  python ../diagnosis-keys/parse_keys_json.py -u -d $f | tr -d '\n' | sed -r 's/,\{\}//g' | python -m json.tool > json/$(basename $f .zip).json
+  python ./parse_keys_json.py -u -d $f | tr -d '\n' | sed -r 's/,\{\}//g' | python -m json.tool > json/$(basename $f .zip).json
+  #python ./parse_keys_json.py -u -d $f > json/$(basename $f .zip).json
 done
