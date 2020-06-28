5a887aa9b8b3eab9107dcf1fbd6b2b9b52bd4eda
janpf <janpf@users.noreply.github.com>
2020-06-28 16:44:04 +0200

    PR for json parser merged

--- create_json.sh
+++ create_json.sh
@@ -2,6 +2,5 @@
 for f in page/keys/*
 do
   echo parsing $f
-  python ./parse_keys_json.py -u -d $f | tr -d '\n' | sed -r 's/,\{\}//g' | python -m json.tool > page/json/$(basename $f .zip).json
-  #python ./parse_keys_json.py -u -d $f > json/$(basename $f .zip).json
+  python ../diagnosis-keys/parse_keys_json.py -d $f > page/json/$(basename $f .zip).json
 done
