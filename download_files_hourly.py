a76c7f5846e7241026aed6a4eb1731cc5d1b882b
janpf <janpf@users.noreply.github.com>
2020-07-05 10:37:05 +0200

    not updating already existing hourly files

--- download_files_hourly.py
+++ download_files_hourly.py
@@ -1,4 +1,5 @@
 import requests
+from pathlib import Path
 
 # https://svc90.main.px.t-online.de/version/v1/diagnosis-keys/country/DE/date
 protocol = "https://"
@@ -7,10 +8,11 @@ version = "v1"
 country = "DE"
 
 dates_url = f"{protocol}{host}/version/{version}/diagnosis-keys/country/{country}/date"
-available_dates = requests.get(dates_url).json()
+available_dates = set(requests.get(dates_url).json())
 print(f"available dates: {available_dates}")
+downloaded_hours_for_dates = {f.stem[: f.stem.rfind("-")] for f in Path("page/keys_hourly").iterdir()}
 
-for date in available_dates:
+for date in available_dates.difference(downloaded_hours_for_dates):
     hours_url = f"{dates_url}/{date}/hour"
     available_hours = requests.get(hours_url).json()
 
