7b131ba7e0940ff387b3bfcb3c30362191f96583
janpf <janpf@users.noreply.github.com>
2020-07-10 22:08:00 +0200

    updating zip files

--- download_files.py
+++ download_files.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import requests
 
 # https://svc90.main.px.t-online.de/version/v1/diagnosis-keys/country/DE/date
@@ -9,7 +10,7 @@ country = "DE"
 dates_url = f"{protocol}{host}/version/{version}/diagnosis-keys/country/{country}/date"
 available_dates = set(requests.get(dates_url).json())
 print(f"available dates: {available_dates}")
-downloaded_dates = {f.stem[: f.stem.rfind("-")] for f in Path("page/keys").iterdir()}
+downloaded_dates = {f.stem for f in Path("page/keys").iterdir()}
 
 for date in available_dates.difference(downloaded_dates):
     print(f"downloading file for {date}")
