e32d6be23498ef9b8eac4ed136a232a5f2a7bec3
janpf <janpf@users.noreply.github.com>
2020-08-19 11:59:41 +0200

    updating logic

--- generate_bash.py
+++ generate_bash.py
@@ -18,7 +18,9 @@ with open("create_users.sh", "w") as cb:
         if f.name == ".gitkeep" or f.name == "removed" or f.name == "updated":
             continue
         if datetime.datetime.fromisoformat(f.stem) >= datetime.datetime.fromisoformat("2020-07-02"):
-            cb.write(f"echo parsing {f}; python ./create_users.py -n -m 9 -d {f} > page/users/{f.stem}.txt\n")
+            cb.write(f"echo parsing {f}; python ./create_users.py -n -a -m 9 -d {f} > page/users/{f.stem}.txt\n")
+        elif "2020-06-23" in f.stem:
+            cb.write(f"echo parsing {f}; python ./create_users.py -m 10 -d {f} > page/users/{f.stem}.txt\n")
         else:
             cb.write(f"echo parsing {f}; python ./create_users.py -d {f} > page/users/{f.stem}.txt\n")
 
@@ -30,11 +32,16 @@ with open("create_users_hourly.sh", "w") as cb:
             continue
 
         if datetime.datetime.fromisoformat(f.stem[: f.stem.rfind("-")]) > datetime.datetime.fromisoformat("2020-07-02"):
-            cb.write(f"echo parsing {f}; python ./create_users.py -n -m 9 -d {f} > page/users_hourly/{f.stem}.txt\n")
+            cb.write(f"echo parsing {f}; python ./create_users.py -n -a -m 9 -d {f} > page/users_hourly/{f.stem}.txt\n")
+
         elif datetime.datetime.fromisoformat(f.stem[: f.stem.rfind("-")]) == datetime.datetime.fromisoformat("2020-07-02"):
             if int(f.stem[f.stem.rfind("-") + 1 :]) > 11:
-                cb.write(f"echo parsing {f}; python ./create_users.py -n -d {f} > page/users_hourly/{f.stem}.txt\n")
+                cb.write(f"echo parsing {f}; python ./create_users.py -n -a -d {f} > page/users_hourly/{f.stem}.txt\n")
             else:
-                cb.write(f"echo parsing {f}; python ./create_users.py -d {f} > page/users_hourly/{f.stem}.txt\n")
+                cb.write(f"echo parsing {f}; python ./create_users.py -a -d {f} > page/users_hourly/{f.stem}.txt\n")
+
+        elif "2020-06-23" in f.stem:
+            cb.write(f"echo parsing {f}; python ./create_users.py -m 10 -d {f} > page/users_hourly/{f.stem}.txt\n")
+
         else:
             cb.write(f"echo parsing {f}; python ./create_users.py -d {f} > page/users_hourly/{f.stem}.txt\n")
