3efc97bdb28415471cdd4398ab98449a83fcc204
janpf <janpf@users.noreply.github.com>
2020-08-19 12:26:57 +0200

    documenting parameters

--- create_users.py
+++ create_users.py
@@ -23,4 +23,5 @@ dk = DiagnosisKeys(dk_file_name)
 dk_list = [DiagnosisKey(tek.key_data, tek.rolling_start_interval_number, tek.rolling_period, tek.transmission_risk_level) for tek in dk.get_keys()]
 
 print("approximated user count according to https://github.com/corona-warn-app/cwa-documentation/issues/258#issuecomment-650700745")
+print(f"called with: multiplier={args.multiplier}, auto_multiplier_detect={args.auto_multiplier_detect}, new_android_apps_only={args.new_android_apps_only}")
 count_users(dk_list, auto_multiplier_detect=args.auto_multiplier_detect, multiplier=args.multiplier, new_android_apps_only=args.new_android_apps_only)
