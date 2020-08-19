c2e7d715c50b983d5b53e277986509afda2893cd
janpf <janpf@users.noreply.github.com>
2020-08-19 11:57:35 +0200

    moving logic to generate_bash.py

--- create_users.py
+++ create_users.py
@@ -11,6 +11,7 @@ from pathlib import Path
 
 parser = argparse.ArgumentParser(description="Exposure Notification Diagnosis Key Parser.", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("-d", "--diagnosiskeys", type=str, default="testExport-2-records-1-of-1.zip", help="file name of the Diagnosis Keys .zip file")
+parser.add_argument("-a", "--auto_multiplier_detect", action="store_true", help="detect the multiplier automatically")
 parser.add_argument("-n", "--new-android-apps-only", action="store_true", help="assume that no 'old' Android apps uploaded keys")
 parser.add_argument("-m", "--multiplier", type=int, default=10, help="padding multiplier (RANDOM_KEY_PADDING_MULTIPLIER as set on cwa-server)")
 args = parser.parse_args()
@@ -22,7 +23,4 @@ dk = DiagnosisKeys(dk_file_name)
 dk_list = [DiagnosisKey(tek.key_data, tek.rolling_start_interval_number, tek.rolling_period, tek.transmission_risk_level) for tek in dk.get_keys()]
 
 print("approximated user count according to https://github.com/corona-warn-app/cwa-documentation/issues/258#issuecomment-650700745")
-if Path(dk_file_name).stem == "2020-06-23":
-    count_users(dk_list, multiplier=10, new_android_apps_only=args.new_android_apps_only)
-else:
-    count_users(dk_list, auto_multiplier_detect=True, multiplier=args.multiplier, new_android_apps_only=args.new_android_apps_only)
+count_users(dk_list, auto_multiplier_detect=args.auto_multiplier_detect, multiplier=args.multiplier, new_android_apps_only=args.new_android_apps_only)
