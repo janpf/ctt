6d43e45294c7da85d85d91e6bf121900fe302531
Jan Pfister <janpf@users.noreply.github.com>
2020-08-28 02:58:47 +0200

    new exception

--- generate_bash.py
+++ generate_bash.py
@@ -31,7 +31,7 @@ with open("create_users_hourly.sh", "w") as cb:
         if f.name == ".gitkeep" or f.name == "removed" or f.name == "updated":
             continue
 
-        if f.stem in ["2020-08-04-9", "2020-08-08-18", "2020-08-08-8", "2020-08-12-10", "2020-08-15-8", "2020-08-18-15", "2020-08-18-19", "2020-08-22-12", "2020-08-24-5", "2020-08-25-10"]:
+        if f.stem in ["2020-08-04-9", "2020-08-08-18", "2020-08-08-8", "2020-08-12-10", "2020-08-15-8", "2020-08-18-15", "2020-08-18-19", "2020-08-22-12", "2020-08-24-5", "2020-08-25-10", "2020-08-27-9"]:
             cb.write(f"echo parsing {f}; python ./create_users.py -n -m 5 -d {f} > page/users_hourly/{f.stem}.txt\n")
 
         elif datetime.datetime.fromisoformat(f.stem[: f.stem.rfind("-")]) > datetime.datetime.fromisoformat("2020-07-02"):
