4b92474f4d229286e47ae948f0145f36122f9ea3
janpf <janpf@users.noreply.github.com>
2020-10-03 15:19:48 +0200

    multiplier doesn't seem to change any time soon

--- generate_bash.py
+++ generate_bash.py
@@ -33,40 +33,8 @@ with open("create_users_hourly.sh", "w") as cb:
         if f.name == ".gitkeep" or f.name == "removed" or f.name == "updated":
             continue
 
-        if f.stem in [
-            "2020-08-04-9",
-            "2020-08-08-8",
-            "2020-08-08-18",
-            "2020-08-12-10",
-            "2020-08-15-8",
-            "2020-08-18-15",
-            "2020-08-18-19",
-            "2020-08-22-12",
-            "2020-08-24-5",
-            "2020-08-25-10",
-            "2020-08-27-9",
-            "2020-08-29-18",
-            "2020-09-06-16",
-            "2020-09-08-10",
-            "2020-09-08-14",
-            "2020-09-11-20",
-            "2020-09-13-11",
-            "2020-09-15-9",
-            "2020-09-15-11",
-            "2020-09-18-14",
-            "2020-09-23-19",
-            "2020-09-24-19",
-            "2020-09-25-6",
-            "2020-09-25-16",
-            "2020-09-26-9",
-            "2020-09-27-12",
-            "2020-10-01-11",
-            "2020-10-02-8"
-        ]:
+        if datetime.datetime.fromisoformat(f.stem[: f.stem.rfind("-")]) > datetime.datetime.fromisoformat("2020-07-02"):
             cb.write(f"echo parsing {f}; python ./create_users.py -n -m 5 -d {f} > page/users_hourly/{f.stem}.txt &\n")
-
-        elif datetime.datetime.fromisoformat(f.stem[: f.stem.rfind("-")]) > datetime.datetime.fromisoformat("2020-07-02"):
-            cb.write(f"echo parsing {f}; python ./create_users.py -n -a -m 5 -d {f} > page/users_hourly/{f.stem}.txt &\n")
 
         elif datetime.datetime.fromisoformat(f.stem[: f.stem.rfind("-")]) == datetime.datetime.fromisoformat("2020-07-02"):
             if int(f.stem[f.stem.rfind("-") + 1 :]) > 11:
