ce3265ae942652b3f6c4e0672103306cfc865902
janpf <janpf@users.noreply.github.com>
2020-10-25 18:47:31 +0100

    providing filehashes and raw keycount (#3)

--- generate_filehashes.py
+++ generate_filehashes.py
@@ -4,14 +4,21 @@ import zipfile
 from pathlib import Path
 
 
-hashes = dict()
+hashes = []
 for zfile in sorted(Path("page/keys/").iterdir(), reverse=True):
     if ".gitkeep" in str(zfile):
         continue
+    data = dict()
+    hashes.append(data)
+
+    data["date"] = zfile.stem
     zip_data = zipfile.ZipFile(zfile)
     infolist = zip_data.infolist()
     with zip_data.open([f for f in infolist if ".bin" in f.filename][0], "r") as bin_file:
-        hashes[hashlib.sha256(bin_file.read()).hexdigest()] = zfile.stem
+        data["hash"] = hashlib.sha256(bin_file.read()).hexdigest()
+
+    with open(Path("page/json/") / (zfile.stem + ".json")) as j_file:
+        data["keyCount"] = json.load(j_file)["keyCount"]
 
 with open("page/json/filehashes.json", "w") as f:
     json.dump(hashes, f, indent=True)
